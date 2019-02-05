629ab5c2406d37246802046ae17ecfcb52ec89c1
Jason Bauer <jbauer23@gmail.com>
2019-02-05 15:50:34 -0800

    Adding my reverse library

--- hello-world.go
+++ hello-world.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/jbauer23/stringutil"
+	"github.com/jbauer23/hello-world/stringutil"
 )
 
 func main() {
 	fmt.Printf(stringutil.Reverse("hello, world\n"))
 
-	fmt.Print("And now I shall print")
+	fmt.Println("And now I shall print")
 }
