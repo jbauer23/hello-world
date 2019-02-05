734d0b51cabaa9b228d85ed740326faf8876ff10
Jason Bauer <jbauer23@gmail.com>
2019-02-05 15:45:38 -0800

    Adding a hello world go script

---
+++ hello-world.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jbauer23/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("hello, world\n"))
+
+	fmt.Print("And now I shall print")
+}
